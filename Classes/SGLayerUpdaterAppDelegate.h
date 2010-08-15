1a2fbe9de07c24f6345eef715e13d8ca3246d94b
dsmith <derek@simplegeo.com>
2010-08-15 14:57:54 -0700

    Removed CoreData from the project.

--- Classes/SGLayerUpdaterAppDelegate.h
+++ Classes/SGLayerUpdaterAppDelegate.h
@@ -3,29 +3,18 @@
 //  SGLayerUpdater
 //
 //  Created by Derek Smith on 8/15/10.
-//  Copyright CrashCorp 2010. All rights reserved.
+//  Copyright SimpleGeo 2010. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
 
 @interface SGLayerUpdaterAppDelegate : NSObject <UIApplicationDelegate> {
     
-    UIWindow *window;
+    UIWindow* window;
     
-@private
-    NSManagedObjectContext *managedObjectContext_;
-    NSManagedObjectModel *managedObjectModel_;
-    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (NSString *)applicationDocumentsDirectory;
 
 @end
 
