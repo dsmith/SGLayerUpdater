7e791daa823c8036b4d4384ba013277c224ac648
dsmith <derek@simplegeo.com>
2010-08-15 15:06:02 -0700

    Initial an SGOAuth token and append it to the SGLocationService.

--- Classes/SGLayerUpdaterAppDelegate.m
+++ Classes/SGLayerUpdaterAppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "SGLayerUpdaterAppDelegate.h"
 
+#import "SGClient.h"
+
 @implementation SGLayerUpdaterAppDelegate
 @synthesize window;
 
@@ -17,11 +19,27 @@
 //////////////////////////////////////////////////////////////////////////////////////////////// 
 
 - (BOOL) application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
-{
+{   
+    NSString* path = [mainBundle pathForResource:@"Token" ofType:@"plist"];
+    NSDictionary* token = [NSDictionary dictionaryWithContentsOfFile:path];
+    
+    NSString* key = [token objectForKey:@"key"];
+    NSString* secret = [token objectForKey:@"secret"];
+    
+    if([key isEqualToString:@"my-secret"] || [secret isEqualToString:@"my-secret"]) {
+        NSLog(@"ERROR!!! - Please change the credentials in Resources/Token.plist");
+        exit(1);
+    }   
+    
+    SGOAuth* token = [[[SGOAuth alloc] initWithKey:key secret:secret] autorelease];
+    SGLocationService* locationService = [SGLocationService sharedLocationService];
+    locationService.HTTPAuthorizer = token;
+
+    // We want to make sure that we are adding the proper credentials to the
+    // location service before we make the window visible. We might end up using
+    // the location service in some of the UIVIew initialization code.
     [window makeKeyAndVisible];
-    
-    
-	
+
 	return YES;
 }
 
